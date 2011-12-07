bd77ba647d91c0f21c9ede4de97dc78b6d1ac1db
elena.moscaliuc <elena.moscaliuc@evozon.com>
2011-12-07 10:30:47 +0200

    MySql and MsSql scripts added
    visual
    :wq

--- src/Snapshot/scripts/7_InsertIntoRoles.sql
+++
@@ -1,8 +0,0 @@
-go INSERT  INTO Roles
-           (Id
-           ,Name
-           ,Description)
-     VALUES
-           ('461e581b-e60b-4dfd-a5a8-88229f14379b'
-           ,N'AllAccess'
-           ,N'This role permits access to the entire application')
