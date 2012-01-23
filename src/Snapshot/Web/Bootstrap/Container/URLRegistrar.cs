aa2e4f52eec2fd6b39076232928821eabe8ae8d6
Claudia Glodan <claudia.glodan@evozon.com>
2012-01-23 12:53:40 +0200

    Send a stock level request by email (STOCKMANAGEMEN-30)

---
+++ src/Snapshot/Web/Bootstrap/Container/URLRegistrar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Autofac;
+using Web.Services.UrlService;
+
+namespace Web.Bootstrap.Container
+{
+    public class URLRegistrar
+    {
+        public static void Register(ContainerBuilder container)
+        {
+            container.RegisterType<URLService>().As<IURLService>();
+        }
+    }
+}
---
+++ src/Snapshot/Web/Services/UrlService/IURLService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Services.UrlService
+{
+    public interface IURLService
+    {
+        string GetEmailLinkUrl(UrlHelper helper, string data);
+    }
+}
