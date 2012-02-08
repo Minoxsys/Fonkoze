2fd98a4743d31545276d4a9ea6c1945e856fac84
Mihai Lazar <sharpoverride@gmail.com>
2012-02-08 09:27:23 +0200

    Added first migrations

---
+++ src/Snapshot/Migrations/1_BasicAuthenticationInfrastructure.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using FluentMigrator;
+using FluentMigrator.Builders.Create.Table;
+
+namespace Migrations
+{
+
+	[Migration(1)]
+	public class BasicAuthenticationInfrastructure : Migration
+	{
+		public override void Up()
+		{
+			
+
+			Create.Table("Permissions").WithCommonColumns()
+				.WithColumn("Name").AsString(ConstraintUtility.NAME_LENGTH).Unique();
+
+			Create.Table("PermissionRoles")
+				.WithColumn("Permission_FK").AsGuid().NotNullable()
+				.WithColumn("Role_FK").AsGuid().NotNullable();
+
+			Create.Table("Roles").WithCommonColumns()
+				.WithColumn("Name").AsString(ConstraintUtility.NAME_LENGTH).Unique()
+				.WithColumn("Description").AsString(ConstraintUtility.DESCRIPTION_LENGTH).Nullable();
+
+			Create.Table("RoleUsers")
+				.WithColumn("User_FK").AsGuid().NotNullable()
+				.WithColumn("Role_FK").AsGuid().NotNullable();
+
+			Create.Table("Users").WithCommonColumns()
+				.WithColumn("UserName").AsString(ConstraintUtility.NAME_LENGTH).NotNullable().Unique()
+				.WithColumn("Password").AsString(ConstraintUtility.NAME_LENGTH).NotNullable()
+				.WithColumn("Email").AsString(ConstraintUtility.NAME_LENGTH).Nullable().Unique()
+				.WithColumn("ClientId").AsGuid();
+
+
+			Create.AddForeignKey("Permissions");
+			Create.AddForeignKey("Roles");
+		}
+
+		public override void Down()
+		{
+			Delete.RemoveForeignKey("Roles");
+			Delete.RemoveForeignKey("Permissions");
+
+			Delete.Table("PermissionRoles");
+			Delete.Table("Permissions");
+			Delete.Table("RoleUsers");
+			Delete.Table("Roles");
+			Delete.Table("Users");
+		}
+  
+		
+
+		
+	}
+}
---
+++ src/Snapshot/Migrations/4_OutpostStockLevels.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentMigrator;
+
+namespace Migrations
+{
+	[Migration(4)]
+	public class OutpostStockLevels : Migration
+	{
+		public override void Down()
+		{
+			Delete.RemoveClientForeignKey("OutpostStockLevels");
+			Delete.RemoveForeignKey("OutpostStockLevels");
+
+			Delete.RemoveClientForeignKey("OutpostHistoricalStockLevels");
+			Delete.RemoveForeignKey("OutpostHistoricalStockLevels");
+
+			Delete.Table("OutpostStockLevels");
+			Delete.Table("OutpostHistoricalStockLevels");
+		}
+
+		public override void Up()
+		{
+
+			Create.Table("OutpostStockLevels")
+				.WithCommonColumns()
+				.WithClientColumn()
+				.WithColumn("OutpostId").AsGuid()
+				.WithColumn("ProductId").AsGuid()
+				.WithColumn("ProductGroupId").AsGuid()
+				.WithColumn("ProductGroupName").AsString(ConstraintUtility.NAME_LENGTH)
+				.WithColumn("ProductName").AsString(ConstraintUtility.NAME_LENGTH)
+				.WithColumn("ProdSMSRef").AsString(20)
+				.WithColumn("StockLevel").AsInt32()
+				.WithColumn("PrevStockLevel").AsInt32()
+				.WithColumn("UdateMethod").AsString(ConstraintUtility.NAME_LENGTH);
+
+
+			Create.AddClientForeignKey("OutpostStockLevels");
+			Create.AddForeignKey("OutpostStockLevels");
+
+			Create.Table("OutpostHistoricalStockLevels")
+				.WithCommonColumns()
+				.WithClientColumn()
+				.WithColumn("OutpostId").AsGuid()
+				.WithColumn("ProductId").AsGuid()
+				.WithColumn("ProductGroupId").AsGuid()
+				.WithColumn("ProductGroupName").AsString(ConstraintUtility.NAME_LENGTH)
+				.WithColumn("ProductName").AsString(ConstraintUtility.NAME_LENGTH)
+				.WithColumn("ProdSMSRef").AsString(20)
+				.WithColumn("StockLevel").AsInt32()
+				.WithColumn("PrevStockLevel").AsInt32()
+				.WithColumn("UdateMethod").AsString(ConstraintUtility.NAME_LENGTH);
+
+			Create.AddClientForeignKey("OutpostHistoricalStockLevels");
+			Create.AddForeignKey("OutpostHistoricalStockLevels");
+
+		}
+	}
+}
