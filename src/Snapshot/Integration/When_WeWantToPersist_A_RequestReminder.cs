792f750acecdbe989ca5ce3bbb633c33e9bedc35
georgian.camarasan <georgian.camarasan@evozon.com>
2012-02-22 10:01:44 +0200

    Added Integration tests for the Schedule and RequestReminder entities.

---
+++ src/Snapshot/Integration/When_WeWantToPersist_A_RequestReminder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Domain;
+using FluentNHibernate.Testing;
+
+namespace IntegrationTests
+{
+    public class When_WeWantToPersist_A_RequestReminder : GivenAPersistenceSpecification<RequestReminder>
+    {
+        private const string PERIOD_TYPE = "Days";
+        private const int PERIOD_VALUE = 1;
+
+        [Test]
+        public void It_Should_Successfully_Persist_A_RequestReminder()
+        {
+            var reminder = Specs
+                .CheckProperty(e => e.PeriodType, PERIOD_TYPE)
+                .CheckProperty(e => e.PeriodValue, PERIOD_VALUE)
+                .VerifyTheMappings();
+
+            Assert.IsNotNull(reminder);
+            Assert.IsInstanceOf<Guid>(reminder.Id);
+
+            session.Delete(reminder);
+            session.Flush();
+        }
+    }
+}
---
+++ src/Snapshot/Integration/When_WeWantToPersist_A_Schedule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Domain;
+using NUnit.Framework;
+using FluentNHibernate.Testing;
+
+namespace IntegrationTests
+{
+    class When_WeWantToPersist_A_Schedule: GivenAPersistenceSpecification<Schedule>
+    {
+        private const string FREQUENCY_TYPE = "Daily";
+        private const int FREQUENCY_VALUE = 3;
+
+        [Test]
+        public void It_Should_Successfully_Persist_A_Schedule()
+        {
+            var schedule = Specs
+                .CheckProperty(e => e.FrequencyType, FREQUENCY_TYPE)
+                .CheckProperty(e => e.FrequencyValue, FREQUENCY_VALUE)
+                .VerifyTheMappings();
+
+            Assert.IsNotNull(schedule);
+            Assert.IsInstanceOf<Guid>(schedule.Id);
+
+            session.Delete(schedule);
+            session.Flush();
+        }
+    }
+}
