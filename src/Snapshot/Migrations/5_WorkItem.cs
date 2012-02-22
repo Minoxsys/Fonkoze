21fe3396e9ddfdb0620fdfa93d768965e9374ca4
Mihai Lazar <mihai.lazar@evozon.com>
2012-02-22 09:04:20 +0200

    Added WebBackgrounder Library Implemented IWorkItemRepository with NHibernate, and added appropriate WorkItemAdapter Created a simple recurring job that also cleans up after itself.

---
+++ src/Snapshot/Migrations/5_WorkItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentMigrator;
+
+namespace Migrations
+{
+    [Migration(5)]
+    public class WorkItemMigration :Migration
+    {
+        public override void Down()
+        {
+            Delete.Table("WorkItems");
+        }
+
+        public override void Up()
+        {
+            Create.Table("WorkItems").WithCommonColumns()
+                .WithColumn("WorkItemId").AsInt64().Identity()
+                .WithColumn("JobName").AsFixedLengthString(64)
+                .WithColumn("WorkerId").AsFixedLengthString(64)
+                .WithColumn("Started").AsDateTime().NotNullable()
+                .WithColumn("Completed").AsDateTime().Nullable()
+                .WithColumn("ExceptionInfo").AsString(8000).Nullable();
+        }
+    }
+}
--- src/Snapshot/Web/Global.asax.cs
+++ src/Snapshot/Web/Global.asax.cs
@@ -9,64 +9,80 @@ using Web.Bootstrap.Routes;
 using Persistence;
 using Autofac;
 using Autofac.Integration.Mvc;
+using WebBackgrounder;
 
 namespace Web
 {
-	// Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-	// visit http://go.microsoft.com/?LinkId=9394801
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+    public class MvcApplication : System.Web.HttpApplication, IContainerAccessor
+    {
+        private static JobManager jobManager;
 
-	public class MvcApplication : System.Web.HttpApplication, IContainerAccessor
-	{
-		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
-		{
-			filters.Add(new HandleErrorAttribute());
-		}
+        private static IContainer container;
 
-
-		protected void Application_Start()
-		{
-			InitializeContainer();
-
-
-			AreaRegistration.RegisterAllAreas();
-
-			RegisterGlobalFilters(GlobalFilters.Filters);
-			RoutesRegistrar.Register();
-		}
+        IContainer IContainerAccessor.Container
+        {
+            get
+            {
+                return container;
+            }
+        }
 
 
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
 
-		protected void Application_BeginRequest(object sender, EventArgs e)
-		{
-		}
+        protected void Application_Start()
+        {
+            InitializeContainer();
 
-		protected void Application_EndRequest(object sender, EventArgs e)
-		{
-		}
+            jobManager = CreateJobWorkersManager();
+            jobManager.Start();
 
-		private static IContainer container;
-		private INHibernateUnitOfWork _unitOfWork;
+            AreaRegistration.RegisterAllAreas();
 
-		IContainer IContainerAccessor.Container
-		{
-			get
-			{
-				return container;
-			}
-		}
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RoutesRegistrar.Register();
+        }
 
-		/// <summary>
-		/// Instantiate the container and add all Controllers that derive from 
-		/// UnityController to the container.  Also associate the Controller 
-		/// with the UnityContainer ControllerFactory.
-		/// </summary>
-		protected virtual void InitializeContainer()
-		{
+        protected void Application_Stop()
+        {
+            jobManager.Stop();
+        }
+
+
+        /// <summary>
+        /// Instantiate the container and add all Controllers that derive from 
+        /// UnityController to the container.  Also associate the Controller 
+        /// with the UnityContainer ControllerFactory.
+        /// </summary>
+        protected static void InitializeContainer()
+        {
             var builder = new ContainerBuilder();
             builder.RegisterControllers(typeof(MvcApplication).Assembly);
             ContainerRegistrar.Register(builder);
             container = builder.Build();
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
-		}
-	}
+        }
+
+        private static JobManager CreateJobWorkersManager()
+        {
+            var jobs = new IJob[]
+            {
+                container.Resolve<Web.BackgroundJobs.EmptyJob>()
+                //new SampleJob(TimeSpan.FromSeconds(35), TimeSpan.FromSeconds(60)),
+                /* new ExceptionJob(TimeSpan.FromSeconds(15)), */
+                //new WorkItemCleanupJob(TimeSpan.FromMinutes(1), TimeSpan.FromMinutes(5), new WorkItemsContext())
+            };
+
+            var coordinator = new WebFarmJobCoordinator(new NHibernateWorkItemRepository(() => container.Resolve<INHibernateSessionFactory>().CreateSession()));
+            var manager = new JobManager(jobs, coordinator);
+
+            //manager.Fail(ex => Elmah.ErrorLog.GetDefault(null).Log(new Error(ex)));
+            return manager;
+        }
+    }
 }
