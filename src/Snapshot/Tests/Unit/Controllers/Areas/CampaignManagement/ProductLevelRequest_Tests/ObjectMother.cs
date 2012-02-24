8c5f43d0f69e601622d973e706da6a5609acb406
Mihai Lazar <mihai.lazar@evozon.com>
2012-02-24 15:56:40 +0200

    Started work on Create product level request

---
+++ src/Snapshot/Tests/Unit/Controllers/Areas/CampaignManagement/ProductLevelRequest_Tests/ObjectMother.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+
+namespace Tests.Unit.Controllers.Areas.CampaignManagement.ProductLevelRequest_Tests
+{
+    public class ObjectMother
+    {
+
+    }
+}
---
+++ src/Snapshot/Web/Areas/CampaignManagement/Controllers/ProductLevelRequestController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Web.Areas.CampaignManagement.Controllers
+{
+    public class ProductLevelRequestController : Controller
+    {
+
+        public ActionResult Overview()
+        {
+            return View();
+        }
+
+    }
+}
