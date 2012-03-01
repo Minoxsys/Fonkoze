6103de63ea05723d250bacf9f259bbbc10911b45
Claudia Glodan <claudia.glodan@evozon.com>
2012-03-01 10:14:51 +0200

    2 regions can have the same name, if there are in different countries

--- src/Snapshot/Web/Areas/OutpostManagement/Controllers/RegionController.cs
+++ src/Snapshot/Web/Areas/OutpostManagement/Controllers/RegionController.cs
@@ -124,7 +124,7 @@ namespace Web.Areas.OutpostManagement.Controllers
 
             var queryRegionValidation = QueryService.Query().Where(p=>p.Client == _client);
 
-            if (queryRegionValidation.Where(it => it.Name == regionInputModel.Name && it.Id != regionInputModel.Id).Count() > 0)
+            if (queryRegionValidation.Where(it => it.Name == regionInputModel.Name && it.Id != regionInputModel.Id && it.Country.Id == regionInputModel.CountryId).Count() > 0)
             {
                 return Json(
                     new ProductJsonActionResponse
