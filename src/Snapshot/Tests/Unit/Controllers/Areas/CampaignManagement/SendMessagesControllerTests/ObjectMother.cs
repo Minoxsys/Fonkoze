e3dec435f32d33b20f9259f46828d836f69821b9
adriana.trif <adriana.trif@evozon.com>
2013-04-22 17:02:20 +0300

    UT for SendMessageToOutpostsMethod

--- src/Snapshot/Tests/Unit/Controllers/Areas/CampaignManagement/SendMessagesControllerTests/ObjectMother.cs
+++ src/Snapshot/Tests/Unit/Controllers/Areas/CampaignManagement/SendMessagesControllerTests/ObjectMother.cs
@@ -7,39 +7,40 @@ using Web.Services;
 using Rhino.Mocks;
 using Core.Persistence;
 using Domain;
+using Web.Areas.CampaignManagement.Controllers;
 
 namespace Tests.Unit.Controllers.Areas.CampaignManagement.SendMessagesControllerTests
 {
     public class ObjectMother
     {
-        public const string SMS_GATEWAY_URL = "https://www.txtlocal.com/sendsmspost.php";
-        public const string SMS_GATEWAY_USER_NAME = "geo_nasa@yahoo.com";
-        public const string SMS_GATEWAY_PASSWORD = "asd123";
-        public const string SMS_GATEWAY_FROM = "xreplyx";
-        public const string SMS_GATEWAY_TESTMODE = "1";
-        public const string SMS_GATEWAY_DEBUG = "0";
-
-        public ISmsGatewaySettingsService fakeSmsGatewaySettingsService;
+       
         public ISendSmsService smsGatewayService;
-        public IQueryOutposts queryOutposts;
-        public IHttpService fakeHttpService;
+        public IQueryService<Outpost> queryOutposts;
         public ISaveOrUpdateCommand<SentSms> SaveOrUpdateCommand;
+
+        public SendMessagesController controller;
+
+        public Outpost outpost;
+        public Guid outpostId ;
+        public string outpostIds;
+
 
         public void SetupSmsService_and_MockServices()
         {
-            fakeHttpService = MockRepository.GenerateMock<IHttpService>();
+            controller = new SendMessagesController();
+            
+            queryOutposts = MockRepository.GenerateMock<IQueryService<Outpost>>();
+            controller.QueryOutposts = queryOutposts;
+            smsGatewayService = MockRepository.GenerateMock<ISendSmsService>();
+            controller.smsGatewayService = smsGatewayService;
+            SaveOrUpdateCommand = MockRepository.GenerateMock<ISaveOrUpdateCommand<SentSms>>();
+            controller.SaveOrUpdateCommand = SaveOrUpdateCommand;
 
-            queryOutposts = MockRepository.GenerateMock<IQueryOutposts>();
-            fakeSmsGatewaySettingsService = MockRepository.GeneratePartialMock<SmsGatewaySettingsService>();
-            fakeSmsGatewaySettingsService.Stub(c => c.SmsGatewayUrl).Return(SMS_GATEWAY_URL);
-            fakeSmsGatewaySettingsService.Stub(c => c.SmsGatewayUserName).Return(SMS_GATEWAY_USER_NAME);
-            fakeSmsGatewaySettingsService.Stub(c => c.SmsGatewayPassword).Return(SMS_GATEWAY_PASSWORD);
-            fakeSmsGatewaySettingsService.Stub(c => c.SmsGatewayFrom).Return(SMS_GATEWAY_FROM);
-            fakeSmsGatewaySettingsService.Stub(c => c.SmsGatewayTestMode).Return(SMS_GATEWAY_TESTMODE);
-            fakeSmsGatewaySettingsService.Stub(c => c.SmsGatewayDebugMode).Return(SMS_GATEWAY_DEBUG);
-            smsGatewayService = new SendSmsService(fakeSmsGatewaySettingsService, fakeHttpService, null);
-
-            queryOutposts = MockRepository.GenerateMock<IQueryOutposts>();
+            outpost = MockRepository.GeneratePartialMock<Outpost>();
+            outpostId = Guid.NewGuid();
+            outpostIds = outpostId + ",";
+            outpost.Stub(o => o.Id).Return(outpostId);
+            outpost.DetailMethod = "123456";
 
         }
 
--- src/Snapshot/Tests/Unit/Controllers/Areas/CampaignManagement/SendMessagesControllerTests/SendMessageToOutpostsMethod.cs
+++ src/Snapshot/Tests/Unit/Controllers/Areas/CampaignManagement/SendMessagesControllerTests/SendMessageToOutpostsMethod.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Domain;
 using NUnit.Framework;
 using Rhino.Mocks;
+using Persistence.Queries.Outposts;
+using Core.Persistence;
+using Web.Models.Shared;
 
 namespace Tests.Unit.Controllers.Areas.CampaignManagement.SendMessagesControllerTests
 {
@@ -14,6 +18,7 @@ namespace Tests.Unit.Controllers.Areas.CampaignManagement.SendMessagesController
     public class SendMessageToOutpostsMethod
     {
         private readonly ObjectMother objectMother = new ObjectMother();
+        private string message = "test";
 
         [SetUp]
         public void BeforeEach()
@@ -22,9 +27,22 @@ namespace Tests.Unit.Controllers.Areas.CampaignManagement.SendMessagesController
 
         }
 
-        public void Should_Return_JSonSuccessMessage_And_SaveIn_SentSmssTable()
+        [Test]
+        public void Should_QueryOutpostsAndCallSendSmsAndReturnJsonActionResponse()
         {
-          //  objectMother.saveOrUpdateProduct.Expect(it => it.Execute(Arg<Product>.Matches(st => st.Name.Equals(objectMother.product.Name))));    
+            objectMother.queryOutposts.Expect(call => call.Query()).Return(new Outpost[] {objectMother.outpost}.AsQueryable());
+            objectMother.smsGatewayService.Expect(call => call.SendSms(objectMother.outpost.DetailMethod, message, false)).Return(null);
+
+            var jsonResult = objectMother.controller.SendMessageToOutposts(objectMother.outpostIds, message);
+
+            objectMother.queryOutposts.VerifyAllExpectations();
+            objectMother.smsGatewayService.VerifyAllExpectations();
+
+            Assert.IsNotNull(jsonResult);
+            Assert.IsInstanceOf<JsonActionResponse>(jsonResult.Data);
+            var jsonData = jsonResult.Data as JsonActionResponse;
+           
+            Assert.AreEqual("Success", jsonData.Status);
         
         }
 
