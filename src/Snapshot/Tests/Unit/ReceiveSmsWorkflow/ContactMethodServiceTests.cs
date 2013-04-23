ea38912f114b07cba05918edcbe10b18cf45cd19
Liviu Mandras <liviu.mandras@evozon.com>
2013-04-23 09:57:13 +0300

    Code cleanup for unit test.

--- src/Snapshot/Tests/Unit/ReceiveSmsWorkflow/ContactMethodServiceTests.cs
+++ src/Snapshot/Tests/Unit/ReceiveSmsWorkflow/ContactMethodServiceTests.cs
@@ -1,12 +1,10 @@
-﻿using System;
+﻿using Core.Persistence;
+using Domain;
+using NUnit.Framework;
+using Rhino.Mocks;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using Core.Persistence;
-using Domain;
-using Rhino.Mocks;
-using NUnit.Framework;
-using Persistence.Queries.Outposts;
 using Web.ReceiveSmsUseCase.Services;
 using Web.Services;
 
@@ -15,18 +13,14 @@ namespace Tests.Unit.ReceiveSmsWorkflow
     [TestFixture]
     public class ContactMethodServiceTests
     {
-
         private IQueryService<Contact> _contactsQueryService;
         private ISaveOrUpdateCommand<Contact> _saveCommandContact;
         private ISaveOrUpdateCommand<Outpost> _saveOrUpdateOutpost;
         private ISendSmsService _smsService;
-        private Contact contact;
-        private Contact contact2;
-        private Outpost outpost;
+        private Contact _contact;
+        private Outpost _outpost;
         private ContactMethodsService _sut;
-        private string phoneNo;
-        private Guid contactId;
-        private Guid contactId2;
+        private string _phoneNo;
 
         [SetUp]
         public void PerTestSetup()
@@ -36,80 +30,73 @@ namespace Tests.Unit.ReceiveSmsWorkflow
             _saveOrUpdateOutpost = MockRepository.GenerateMock<ISaveOrUpdateCommand<Outpost>>();
             _smsService = MockRepository.GenerateMock<ISendSmsService>();
 
-            phoneNo="1234567";
+            _phoneNo = "1234567";
 
-            Guid contactId = Guid.NewGuid();
-            contact = MockRepository.GeneratePartialMock<Contact>();
-            contact.ContactDetail = phoneNo;
-            contact.ContactType = Contact.MOBILE_NUMBER_CONTACT_TYPE;
-            contact.IsMainContact = false;
-            contact.Stub(c => c.Id).Return(contactId);
-            
-            Guid contactId2 = Guid.NewGuid();
-            contact2 = MockRepository.GeneratePartialMock<Contact>();
+            var contactId = Guid.NewGuid();
+            _contact = MockRepository.GeneratePartialMock<Contact>();
+            _contact.ContactDetail = _phoneNo;
+            _contact.ContactType = Contact.MOBILE_NUMBER_CONTACT_TYPE;
+            _contact.IsMainContact = false;
+            _contact.Stub(c => c.Id).Return(contactId);
+
+            var contactId2 = Guid.NewGuid();
+            var contact2 = MockRepository.GeneratePartialMock<Contact>();
             contact2.ContactDetail = "67890";
             contact2.ContactType = Contact.MOBILE_NUMBER_CONTACT_TYPE;
             contact2.IsMainContact = true;
             contact2.Stub(c => c.Id).Return(contactId2);
 
-            List<Contact> contactLst = new List<Contact>();
-            contactLst.Add(contact);
-            contactLst.Add(contact2);
-            outpost = MockRepository.GeneratePartialMock<Outpost>();
-            outpost.Contacts = contactLst;
-            
+            var contactLst = new List<Contact> {_contact, contact2};
+            _outpost = MockRepository.GeneratePartialMock<Outpost>();
+            _outpost.Contacts = contactLst;
+
             _sut = new ContactMethodsService(_contactsQueryService, _saveCommandContact, _saveOrUpdateOutpost, _smsService);
-
-            
         }
-
 
         [Test]
         public void ActivatePhoneNumber_UpdatesContact()
         {
 
-           _contactsQueryService.Expect(call => call.Query()).Return(new Contact[] { contact }.AsQueryable());
+            _contactsQueryService.Expect(call => call.Query()).Return(new[] {_contact}.AsQueryable());
 
-           _saveCommandContact.Expect(call => call.Execute(Arg<Contact>.Matches(c =>
-                                                                           c.ContactDetail == phoneNo &&
-                                                                           c.ContactType == Contact.MOBILE_NUMBER_CONTACT_TYPE &&
-                                                                           c.IsMainContact)
-                                                                           ));
-           _saveCommandContact.Expect(call => call.Execute(Arg<Contact>.Matches(c =>
-                                                                          c.ContactDetail == "67890" &&
-                                                                          c.ContactType == Contact.MOBILE_NUMBER_CONTACT_TYPE &&
-                                                                          c.IsMainContact==false)
-                                                                          ));
-           _sut.ActivatePhoneNumber(phoneNo, outpost);
-         
+            _saveCommandContact.Expect(call => call.Execute(Arg<Contact>.Matches(c =>
+                                                                                 c.ContactDetail == _phoneNo &&
+                                                                                 c.ContactType == Contact.MOBILE_NUMBER_CONTACT_TYPE &&
+                                                                                 c.IsMainContact)
+                                                   ));
+            _saveCommandContact.Expect(call => call.Execute(Arg<Contact>.Matches(c =>
+                                                                                 c.ContactDetail == "67890" &&
+                                                                                 c.ContactType == Contact.MOBILE_NUMBER_CONTACT_TYPE &&
+                                                                                 c.IsMainContact == false)
+                                                   ));
+            _sut.ActivatePhoneNumber(_phoneNo, _outpost);
+
             _contactsQueryService.VerifyAllExpectations();
-           _saveCommandContact.VerifyAllExpectations();
+            _saveCommandContact.VerifyAllExpectations();
 
         }
 
         [Test]
         public void ActivatePhoneNumber_UpdatesOutpost()
         {
+            _contactsQueryService.Expect(call => call.Query()).Return(new[] {_contact}.AsQueryable());
+            _saveOrUpdateOutpost.Expect(call => call.Execute(Arg<Outpost>.Matches(o => o.DetailMethod == _phoneNo)));
 
-            _contactsQueryService.Expect(call => call.Query()).Return(new Contact[] { contact }.AsQueryable());
-            _saveOrUpdateOutpost.Expect(call => call.Execute(Arg<Outpost>.Matches(o => o.DetailMethod == phoneNo)));
-
-            _sut.ActivatePhoneNumber(phoneNo,outpost);
+            _sut.ActivatePhoneNumber(_phoneNo, _outpost);
 
             _contactsQueryService.VerifyAllExpectations();
             _saveOrUpdateOutpost.VerifyAllExpectations();
-
         }
-
 
         [Test]
-        public void ActivatePhoneNumber_CallsSendSms()
+        public void ActivatePhoneNumber_SendsSmsConfirmationMessageBackToSender()
         {
-            _contactsQueryService.Expect(call => call.Query()).Return(new Contact[] { contact }.AsQueryable());
-            _smsService.Expect(call => call.SendSms(phoneNo, "Activated!", true)).Return(null);
-            _sut.ActivatePhoneNumber(phoneNo, outpost);
+            _contactsQueryService.Expect(call => call.Query()).Return(new[] {_contact}.AsQueryable());
+            _smsService.Expect(call => call.SendSms(_phoneNo, "Activated!", true)).Return(null);
+
+            _sut.ActivatePhoneNumber(_phoneNo, _outpost);
+
             _smsService.VerifyAllExpectations();
         }
-
     }
 }
