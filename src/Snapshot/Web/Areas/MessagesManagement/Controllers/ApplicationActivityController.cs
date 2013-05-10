dc672c55d0752cf3136c5db8617ff60f838cd21b
Liviu Mandras <liviu.mandras@evozon.com>
2013-05-10 15:57:07 +0300

    FONKOZEHAITI-29 As a manager I want the activity report to contain all activity on the application so I can see any detail I may need [added tab to the sidebar and renamed all others]

---
+++ src/Snapshot/Web/Areas/MessagesManagement/Controllers/ApplicationActivityController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web.Security;
+
+namespace Web.Areas.MessagesManagement.Controllers
+{
+    public class ApplicationActivityController : Controller
+    {
+        [HttpGet]
+        [Requires(Permissions = "Messages.View")]
+        public ActionResult Overview()
+        {
+            return View("Overview");
+        }
+    }
+}
