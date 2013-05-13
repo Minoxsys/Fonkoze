1d17d24f42a3a0663229f426212f7b08a37aeb04
adriana.trif <adriana.trif@evozon.com>
2013-05-13 13:47:03 +0300

    Product Sales Report - chart groundwork

---
+++ src/Snapshot/Web/Areas/StockAdministration/Models/HistoricalProductLevel/ProductSaleChartModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Areas.StockAdministration.Models.HistoricalProductLevel
+{
+    public class ProductSaleChartModel
+    {
+        public string Day { get; set; }
+        public int Quantity { get; set; }
+        public string Date { get; set; }
+    }
+}
