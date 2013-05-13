f20d3117ee4fead7e7337c5737a30c94a4a931d9
Liviu Mandras <liviu.mandras@evozon.com>
2013-05-13 11:44:08 +0300

    Added favicon

--- src/Snapshot/Web/Bootstrap/Routes/IgnoredRoutesRegistrar.cs
+++ src/Snapshot/Web/Bootstrap/Routes/IgnoredRoutesRegistrar.cs
@@ -1,19 +1,15 @@
-﻿using System.Web;
-using System.Web.Routing;
+﻿using System.Web.Routing;
 using System.Web.Mvc;
 
 namespace Web.Bootstrap.Routes
 {
-	public class IgnoredRoutesRegistrar
-	{
-
-		public static void Register( RouteCollection routes )
-		{
-
-			routes.IgnoreRoute("favicon.ico");
-			routes.IgnoreRoute("Assets/{*path}");
-			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-		}
-	}
+    public class IgnoredRoutesRegistrar
+    {
+        public static void Register(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{*favicon}", new {favicon = @"(.*/)?favicon.ico(/.*)?"});
+            routes.IgnoreRoute("Assets/{*path}");
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+        }
+    }
 }
