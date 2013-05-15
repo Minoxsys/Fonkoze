ddfba11ab6e678bd36db4e31fcedbcf2a9caa6d3
adriana.trif <adriana.trif@evozon.com>
2013-05-15 11:49:30 +0300

    UT for DoAfterStockUpdate

--- src/Snapshot/Tests/Unit/ReceiveSmsWorkflow/Commands/StockSaleMessageCommandTests.cs
+++ src/Snapshot/Tests/Unit/ReceiveSmsWorkflow/Commands/StockSaleMessageCommandTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using Domain;
 using Moq;
+using System.Linq;
 using NUnit.Framework;
 using Tests.Unit.ReceiveSmsWorkflow.Base;
 using Web.Models.Parsing;
@@ -40,10 +41,16 @@ namespace Tests.Unit.ReceiveSmsWorkflow.Commands
             l.Add(new ParsedProduct(){ProductGroupCode="ll",ProductCode="er",StockLevel=2,ClientIdentifier="f"});
             l.Add(new ParsedProduct(){ProductGroupCode="ll",ProductCode="be",StockLevel=2,ClientIdentifier="n"});
             var parseResult = new SmsParseResult { ParsedProducts = l };
+            ProductQueryServiceMock.Setup(s => s.Query()).Returns(new Product[] { new Product() { SMSReferenceCode = "er" }, new Product() { SMSReferenceCode = "be" } }.AsQueryable());
             //Act    
             Sut.DoAfterStockUpdate(parseResult, OutpostMock.Object);
             //Assert
-            //SaveProductSaleCmdMock.Verify(cmd=>cmd.Execute(It.Is<ProductSale>(ps=>ps.Outpost==OutpostMock.Object && ps.Product)))
+            SaveProductSaleCmdMock.Verify(cmd => cmd.Execute(It.Is<ProductSale>(ps => ps.Outpost == OutpostMock.Object
+                                                                            && ps.Product.SMSReferenceCode == "er"
+                                                                            && ps.Quantity == 2 && ps.ClientIdentifier == "f")));
+            SaveProductSaleCmdMock.Verify(cmd => cmd.Execute(It.Is<ProductSale>(ps => ps.Outpost == OutpostMock.Object
+                                                                           && ps.Product.SMSReferenceCode == "be"
+                                                                           && ps.Quantity == 2 && ps.ClientIdentifier == "n")));
 
 
         }
