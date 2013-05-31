0eb109f0f3e1184fceb0008670e57b8373c219c1
andrei.spatar <andrei.spatar@evozon.com>
2013-05-31 09:57:02 +0300

    FONKOZEHAITI-142 Request messages generated by Product Level Request campaign needs updates best on our new SMS formats

--- src/Snapshot/Web/Services/FormattingStrategy.cs
+++ src/Snapshot/Web/Services/FormattingStrategy.cs
@@ -7,7 +7,7 @@ namespace Web.Services
     public class FormattingStrategy
     {
 
-        private const string SMS_MESSAGE_TEMPLATE = "Please provide current stock level for product group {0} using format\n{1} {2}";
+        private const string SMS_MESSAGE_TEMPLATE = "Please provide current stock level for product group {0} using format\n SC {1}";
 
         public string FormatEmail(string productGroupName, string link)
         {
@@ -18,8 +18,8 @@ namespace Web.Services
         public string FormatSms(ProductLevelRequestMessageInput input)
         {
             StringBuilder productReferenceCodes = new StringBuilder();
-            input.Products.ForEach(p => productReferenceCodes.Append(p.SMSReferenceCode + "0"));
-            string smsMessage = string.Format(SMS_MESSAGE_TEMPLATE, input.ProductGroup.Name, input.ProductGroup.ReferenceCode, productReferenceCodes);
+            input.Products.ForEach(p => productReferenceCodes.Append(p.SMSReferenceCode + "0 "));
+            string smsMessage = string.Format(SMS_MESSAGE_TEMPLATE, input.ProductGroup.Name, productReferenceCodes);
             return smsMessage;
         }
     }
