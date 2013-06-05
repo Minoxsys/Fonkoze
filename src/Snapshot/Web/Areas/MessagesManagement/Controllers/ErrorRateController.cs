25449bf047b148466e3479bae59995dff8cfe082
Liviu Mandras <liviu.mandras@evozon.com>
2013-06-05 17:05:57 +0300

    FONKOZEHAITI-147 Add error rates to an existing report or in a new report

---
+++ src/Snapshot/Web/Areas/MessagesManagement/Controllers/ErrorRateController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Core.Persistence;
+using Domain;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web.Mvc;
+using Web.Areas.MessagesManagement.Models.ErrorRate;
+using Web.Models.Shared;
+using Web.Security;
+
+namespace Web.Areas.MessagesManagement.Controllers
+{
+    public class ErrorRateController : Controller
+    {
+        private readonly IQueryService<RawSmsReceived> _rawSmsQueryService;
+        private readonly IQueryService<Outpost> _outpostQueryService;
+
+        public ErrorRateController(IQueryService<RawSmsReceived> rawSmsQueryService, IQueryService<Outpost> outpostQueryService)
+        {
+            _outpostQueryService = outpostQueryService;
+            _rawSmsQueryService = rawSmsQueryService;
+        }
+
+        [HttpGet]
+        [Requires(Permissions = "Messages.View")]
+        public ActionResult Overview()
+        {
+            return View();
+        }
+
+        public ActionResult GetErrorRateItems(IndexTableInputModel inputModel)
+        {
+            Debug.Assert(inputModel.limit != null, "inputModel.limit != null");
+            var pageSize = inputModel.limit.Value;
+
+            var rawDataQuery = _rawSmsQueryService.Query().ToList().GroupBy(sms => sms.Sender);
+            var errorRates = new List<ErrorRateViewModel>();
+            foreach (IGrouping<string, RawSmsReceived> rawSmsGroup in rawDataQuery)
+            {
+                var outpost = _outpostQueryService.Query().FirstOrDefault(o => o.DetailMethod == rawSmsGroup.Key);
+                int incorrectMsg = rawSmsGroup.Count(s => !s.ParseSucceeded);
+                int totalMsg = rawSmsGroup.Count();
+
+                errorRates.Add(new ErrorRateViewModel
+                    {
+                        SellerName = outpost != null ? outpost.Name : "-",
+                        ErrorMessages = incorrectMsg,
+                        TotalMessages = totalMsg,
+                        ErrorRate = (int) ((double) incorrectMsg/totalMsg*100),
+                        Sender = rawSmsGroup.Key
+                    });
+            }
+
+            var errorRateQuery = errorRates.AsQueryable();
+
+
+            var orderByColumnDirection = new Dictionary<string, Func<IQueryable<ErrorRateViewModel>>>
+                {
+                    {"Sender-ASC", () => errorRateQuery.OrderBy(c => c.Sender)},
+                    {"Sender-DESC", () => errorRateQuery.OrderByDescending(c => c.Sender)},
+                    {"SellerName-ASC", () => errorRateQuery.OrderBy(c => c.SellerName)},
+                    {"SellerName-DESC", () => errorRateQuery.OrderByDescending(c => c.SellerName)},
+                    {"ErrorMessages-ASC", () => errorRateQuery.OrderBy(c => c.ErrorMessages)},
+                    {"ErrorMessages-DESC", () => errorRateQuery.OrderByDescending(c => c.ErrorMessages)},
+                    {"TotalMessages-ASC", () => errorRateQuery.OrderBy(c => c.TotalMessages)},
+                    {"TotalMessages-DESC", () => errorRateQuery.OrderByDescending(c => c.TotalMessages)},
+                    {"ErrorRate-ASC", () => errorRateQuery.OrderBy(c => c.ErrorRate)},
+                    {"ErrorRate-DESC", () => errorRateQuery.OrderByDescending(c => c.ErrorRate)},
+                };
+
+            errorRateQuery = orderByColumnDirection[String.Format("{0}-{1}", inputModel.sort, inputModel.dir)].Invoke();
+
+            if (!string.IsNullOrEmpty(inputModel.searchValue))
+                errorRateQuery = errorRateQuery.Where(it => it.SellerName.Contains(inputModel.searchValue));
+
+            var totalItems = errorRateQuery.Count();
+
+            Debug.Assert(inputModel.start != null, "inputModel.start != null");
+            errorRateQuery = errorRateQuery
+                .Take(pageSize)
+                .Skip(inputModel.start.Value);
+
+            return Json(new
+                {
+                    Items = errorRateQuery.ToArray(),
+                    TotalItems = totalItems
+                }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ src/Snapshot/Web/Areas/MessagesManagement/Models/ErrorRate/ErrorRateViewModel.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Web.Areas.MessagesManagement.Models.ErrorRate
+{
+    public class ErrorRateViewModel
+    {
+        public string Sender { get; set; }
+        public string SellerName { get; set; }
+        public int ErrorMessages { get; set; }
+        public int TotalMessages { get; set; }
+        public int ErrorRate { get; set; }
+    }
+}
